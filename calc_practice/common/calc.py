8ecb55bd43a1cf1b8c1b89145160464603b39ffd
wanghao <13281276736@189.com>
2021-01-31 17:32:30 +0800

    作业-计算器

--- calc_practice/common/calc.py
+++ calc_practice/common/calc.py
@@ -9,17 +9,17 @@
 class Calculator:
 
     #加法
-    def add(self,a,b):
+    def add(self, a, b):
         return a + b
 
     #减法
-    def sub(self,a,b):
+    def sub(self, a, b):
         return a - b
 
     #乘法
-    def mul(self,a,b):
+    def mul(self, a, b):
         return a * b
 
     #除法
-    def div(self,a,b):
+    def div(self, a, b):
         return a / b
---
+++ calc_practice/testcases/test_calc.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2021/1/30 0030 21:30
+@Auth ： wanghao
+@File ：test_calc.py
+"""
+import pytest
+import yaml
+from calc_practice.common.calc import Calculator
+
+
+with open("../datas/calc.yaml") as f:
+    data = yaml.safe_load(f)
+    #获取add数据
+    add_datas = data['add']['datas']
+    #获取div数据
+    div_datas = data['div']['datas']
+    myid = data['add']['myid']
+
+class TestCalcutor:
+    #所有用例开始前执行
+    def setup_class(self):
+        self.calc = Calculator()
+        print("计算开始")
+
+    #所有用例执行完后执行
+    def teardown_class(self):
+        print("用例执行结束")
+
+    @pytest.mark.parametrize("a,b,expect", add_datas)
+    def test_add(self, a, b, expect):
+        #调用加法
+        result = self.calc.add(a, b)
+        if isinstance(result, float):
+            result = round(result, 2)
+        assert result == expect
+
+    @pytest.mark.parametrize("a,b,expect", div_datas)
+    def test_div(self, a, b, expect):
+
+        if b == 0:
+            print("被除数不能为零")
+            assert False
+        #调用除法
+        result = self.calc.div(a, b)
+        if isinstance(result, float):
+            result = round(result, 2)
+        assert result == expect
+
+
